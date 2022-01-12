49b0575d7a06bd40d2a6a820b1ac7e8e88eafde8
idk <hankhill19580@gmail.com>
2022-01-12 16:52:35 -0500

    Start looking at where to have sinks

--- apps/i2ptunnel/java/src/net/i2p/i2ptunnel/udpTunnel/I2PTunnelUDPClient.java
+++ apps/i2ptunnel/java/src/net/i2p/i2ptunnel/udpTunnel/I2PTunnelUDPClient.java
@@ -1,31 +1,54 @@
 package net.i2p.i2ptunnel.udpTunnel;
 
-import net.i2p.i2ptunnel.streamr.Pinger;
+import java.net.InetAddress;
+
+import net.i2p.i2ptunnel.I2PTunnel;
+import net.i2p.i2ptunnel.Logging;
+//import net.i2p.i2ptunnel.streamr.Pinger;
 import net.i2p.i2ptunnel.udp.UDPSink;
+import net.i2p.util.EventDispatcher;
+
+/**
+ * This is a UDP "Client" which has only a "Sink" for sending
+ * datagrams to I2P from a UDP source.
+ *
+ * @author idk
+ */
 
 public class I2PTunnelUDPClient extends I2PTunnelUDPClientBase {
+    private final UDPSink sink;
+    // private final Pinger pinger;
 
-    public I2PTunnelUDPClient(String host, int port, String destination, Logging l, EventDispatcher notifyThis, I2PTunnel tunnel) {
-        super(host, port, destination, l, notifyThis, tunnel);
+    public I2PTunnelUDPClient(String host, int port, String destination, Logging l, EventDispatcher notifyThis,
+            I2PTunnel tunnel) {
+        // super(host, port,
+        super(destination, l, notifyThis, tunnel);
+        UDPSink _sink = null;
+        try {
+            _sink = new UDPSink(InetAddress.getByName(host), port);
+        } catch (Exception e) {
+            _sink = null;
+            // TODO: Log an error and indicate that the tunnel will fail to start.
+        } finally {
+            this.sink = _sink;
+        }
+        // this.pinger = new Pinger(this.sink);
+
     }
 
     @Override
     public final void startRunning() {
         super.startRunning();
         // send subscribe-message
-        this.pinger.start();
+        // this.pinger.start();
         l.log("I2PTunnelUDPClient client ready");
     }
 
     @Override
     public boolean close(boolean forced) {
         // send unsubscribe-message
-        this.pinger.stop();
+        // this.pinger.stop();
         this.sink.stop();
         return super.close(forced);
     }
-
-    private final UDPSink sink;
-    private final Pinger pinger;
 }
-
--- apps/i2ptunnel/java/src/net/i2p/i2ptunnel/udpTunnel/I2PTunnelUDPServer.java
+++ apps/i2ptunnel/java/src/net/i2p/i2ptunnel/udpTunnel/I2PTunnelUDPServer.java
@@ -1,31 +1,71 @@
 package net.i2p.i2ptunnel.udpTunnel;
 
+import java.io.File;
+import java.net.InetAddress;
+
+import net.i2p.i2ptunnel.I2PTunnel;
+import net.i2p.i2ptunnel.Logging;
 import net.i2p.i2ptunnel.streamr.Pinger;
 import net.i2p.i2ptunnel.udp.UDPSink;
+import net.i2p.i2ptunnel.udp.UDPSource;
+import net.i2p.util.EventDispatcher;
+import net.i2p.util.Log;
 
+/**
+ * This is a UDP "Server" which is a UDP tunnel which has both a "Sink" for
+ * sending and a "Source" for receiving.
+ *
+ * @author idk
+ */
 public class I2PTunnelUDPServer extends I2PTunnelUDPServerBase {
+    private final Log _log = new Log(I2PTunnelUDPServer.class);
+    private final UDPSink sink;
+    private final UDPSource source;
+    private final InetAddress UDP_HOSTNAME;
+    private final int UDP_PORT;
 
-        public I2PTunnelUDPServer(String host, int port, String destination, Logging l, EventDispatcher notifyThis, I2PTunnel tunnel) {
-            super(host, port, destination, l, notifyThis, tunnel);
+    public I2PTunnelUDPServer(String host, int port, File privkey, String privkeyname, Logging l,
+            EventDispatcher notifyThis,
+            I2PTunnel tunnel) {
+        super(privkey, privkeyname, l, notifyThis, tunnel);
+        // File privkey, String privkeyname, Logging l,EventDispatcher notifyThis,
+        // I2PTunnel tunnel
+        InetAddress _udpHostname = null;
+        try {
+            _udpHostname = InetAddress.getByName(host);
+        } catch (Exception e) {
+            if (_log.shouldLog(Log.WARN))
+                _log.warn("Failed to resolve hostname, using default(localhost): " + host, e);
+            try {
+                _udpHostname = InetAddress.getLocalHost();
+            } catch (Exception crite) {
+                if (_log.shouldLog(Log.ERROR))
+                    _log.warn("Failed to resolve localhost, UDP tunnel will fail.: " + host, crite);
+                _udpHostname = null;
+            }
+        } finally {
+            if (_udpHostname == null) {
+                _log.error("Failed to resolve UDP hostname: " + host);
+            }
         }
+        this.UDP_HOSTNAME = _udpHostname;
+        this.UDP_PORT = port;
+        this.sink = new UDPSink(this.UDP_HOSTNAME, this.UDP_PORT);
 
-        @Override
-        public final void startRunning() {
-            super.startRunning();
-            // send subscribe-message
-            this.pinger.start();
-            l.log("I2PTunnelUDPServer server ready");
-        }
+        this.source = new UDPSource(this.UDP_PORT);
+    }
 
-        @Override
-        public boolean close(boolean forced) {
-            // send unsubscribe-message
-            this.pinger.stop();
-            this.sink.stop();
-            return super.close(forced);
-        }
+    @Override
+    public final void startRunning() {
+        super.startRunning();
+        // send subscribe-message
+        l.log("I2PTunnelUDPServer server ready");
+    }
 
-        private final UDPSink sink;
-        private final Pinger pinger;
-
+    @Override
+    public boolean close(boolean forced) {
+        // send unsubscribe-message
+        this.sink.stop();
+        return super.close(forced);
+    }
 }
