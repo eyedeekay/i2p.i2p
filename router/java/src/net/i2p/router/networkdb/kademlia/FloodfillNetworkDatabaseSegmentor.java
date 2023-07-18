c90b647ff68c9d3a889d89d8d30eedc0389d9fa4
eyedeekay <idk@mulder>
2023-07-18 16:52:37 -0400

    Router: don't initialize a subNetDb if it's already initialized

--- router/java/src/net/i2p/router/networkdb/kademlia/FloodfillNetworkDatabaseSegmentor.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/FloodfillNetworkDatabaseSegmentor.java
@@ -48,14 +48,15 @@ public class FloodfillNetworkDatabaseSegmentor extends SegmentedNetworkDatabaseF
         if (subdb == null) {
             subdb = new FloodfillNetworkDatabaseFacade(_context, id);
             _subDBs.put(id, subdb);
-            subdb.startup();
         }
+        if (!subdb.isInitialized())
+            subdb.startup();
         return subdb;
     }
 
     public synchronized void startup() {
         for (FloodfillNetworkDatabaseFacade subdb : _subDBs.values()) {
-            if (subdb.isInitialized()){
+            if (!subdb.isInitialized()){
                 subdb.startup();
             }
         }
