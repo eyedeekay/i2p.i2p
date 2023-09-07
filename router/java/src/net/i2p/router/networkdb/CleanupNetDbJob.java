b7202e47fc75f88ed3e7f17a638cbd72a7304716
eyedeekay <idk@mulder>
2023-09-06 21:04:33 -0400

    Router: set the netDb cleanup jobs to happen every minute

--- router/java/src/net/i2p/router/networkdb/CleanupNetDbJob.java
+++ router/java/src/net/i2p/router/networkdb/CleanupNetDbJob.java
@@ -1,9 +1,12 @@
-package net.i2p.router;
+package net.i2p.router.networkdb;
 
+import net.i2p.router.JobImpl;
+import net.i2p.router.RouterContext;
 import net.i2p.router.networkdb.kademlia.FloodfillNetworkDatabaseSegmentor;
 
 public class CleanupNetDbJob extends JobImpl {
     private final RouterContext ctx;
+    private static final long RERUN_DELAY_MS = 1 * 60 * 1000;
 
     public CleanupNetDbJob(RouterContext context) {
         super(context);
@@ -19,6 +22,7 @@ public class CleanupNetDbJob extends JobImpl {
     public void runJob() {
         FloodfillNetworkDatabaseSegmentor fnds = (FloodfillNetworkDatabaseSegmentor) ctx.netDb();
         fnds.removeDeadSubDbs(ctx.clientManager().listClients());
+        requeue(RERUN_DELAY_MS);
     }
 
 }
