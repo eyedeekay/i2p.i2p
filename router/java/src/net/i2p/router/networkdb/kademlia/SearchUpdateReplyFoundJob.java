3da6c22ae64b81e92c9e03b416e133bcd2121d43
eyedeekay <idk@mulder>
2023-09-15 11:54:26 -0400

    Router: just use _facade instead of looking up the netDb with the dbid

--- router/java/src/net/i2p/router/networkdb/kademlia/SearchUpdateReplyFoundJob.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/SearchUpdateReplyFoundJob.java
@@ -88,11 +88,11 @@ class SearchUpdateReplyFoundJob extends JobImpl implements ReplyJob {
                 switch (entry.getType()) {
                     case DatabaseEntry.KEY_TYPE_ROUTERINFO:
                         RouterInfo ri = (RouterInfo) entry;
-                        getContext().netDbSegmentor().getSubNetDB(_facade._dbid).store(ri.getHash(), ri);
+                        _facade.store(ri.getHash(), ri);
                         break;
                     case DatabaseEntry.KEY_TYPE_LEASESET:
                         LeaseSet ls = (LeaseSet) entry;
-                        getContext().netDbSegmentor().getSubNetDB(_facade._dbid).store(ls.getHash(), ls);
+                        _facade.store(ls.getHash(), ls);
                         break;
                     default:
                         break;
